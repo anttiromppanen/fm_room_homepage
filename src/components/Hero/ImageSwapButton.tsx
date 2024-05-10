f9b1f1f2445af63b27892fb99b2c238bf53b92ad
Antti Romppanen <romppanen.antti@gmail.com>
2024-05-10 17:05:20 +0300

    added hover effects

--- src/components/Hero/ImageSwapButton.tsx
+++ src/components/Hero/ImageSwapButton.tsx
@@ -1,6 +1,26 @@
 import leftAngle from "../../assets/images/icon-angle-left.svg";
 import rightAngle from "../../assets/images/icon-angle-right.svg";
 
+function Button({
+  handleClick,
+  img,
+  altText,
+}: {
+  handleClick: () => void;
+  img: string;
+  altText: string;
+}) {
+  return (
+    <button
+      type="button"
+      onClick={handleClick}
+      className="flex flex-1 items-center justify-center hover:bg-userVeryDarkGray focus:outline-orange-500"
+    >
+      <img src={img} alt={altText} />
+    </button>
+  );
+}
+
 function ImageSwapButton({
   handlePreviousImage,
   handleNextImage,
@@ -10,20 +30,16 @@ function ImageSwapButton({
 }) {
   return (
     <div className="flex h-[56px] w-[112px] bg-black md:h-[80px] md:w-[28.4%]">
-      <button
-        type="button"
-        onClick={handlePreviousImage}
-        className="flex flex-1 items-center justify-center"
-      >
-        <img src={leftAngle} alt="previous product" />
-      </button>
-      <button
-        type="button"
-        onClick={handleNextImage}
-        className="flex flex-1 items-center justify-center"
-      >
-        <img src={rightAngle} alt="next product" />
-      </button>
+      <Button
+        img={leftAngle}
+        altText="previous product"
+        handleClick={handlePreviousImage}
+      />
+      <Button
+        img={rightAngle}
+        altText="next product"
+        handleClick={handleNextImage}
+      />
     </div>
   );
 }
